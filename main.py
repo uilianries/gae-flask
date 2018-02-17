b92e2dac1c7bf20b819a7b7602921b59e6227554
Uilian Ries <uilianries@gmail.com>
2018-02-17 21:11:57 -0200

    Replace entrypoint
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- main.py
+++ main.py
@@ -1,106 +1,49 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""This sample shows how to connect to PostgreSQL running on Cloud SQL.
-
-See the documentation for details on how to setup and use this sample:
-    https://cloud.google.com/appengine/docs/flexible/python\
-    /using-cloud-sql-postgres
-"""
-
-import datetime
 import logging
-import os
-import socket
 
-from flask import Flask, request
+from flask import Flask, render_template, request
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
-
-def is_ipv6(addr):
-    """Checks if a given address is an IPv6 address."""
-    try:
-        socket.inet_pton(socket.AF_INET6, addr)
-        return True
-    except socket.error:
-        return False
-
-
-# [START example]
-# Environment variables are defined in app.yaml.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-class Visit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime())
-    user_ip = db.Column(db.String(46))
-
-    def __init__(self, timestamp, user_ip):
-        self.timestamp = timestamp
-        self.user_ip = user_ip
+import models
 
 db.drop_all()
 db.create_all()
 
 @app.route('/')
-def index():
-    user_ip = request.remote_addr
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
-    # Keep only the first two octets of the IP address.
-    if is_ipv6(user_ip):
-        user_ip = ':'.join(user_ip.split(':')[:2])
-    else:
-        user_ip = '.'.join(user_ip.split('.')[:2])
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
 
-    visit = Visit(
-        user_ip=user_ip,
-        timestamp=datetime.datetime.utcnow()
-    )
-
-    db.session.add(visit)
+    new_user = models.User(name=name, email=email, url=site, comments=comments)
+    db.session.add(new_user)
     db.session.commit()
 
-    visits = Visit.query.order_by(sqlalchemy.desc(Visit.timestamp)).limit(10)
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
 
-    results = [
-        'Time: {} Addr: {}'.format(x.timestamp, x.user_ip)
-        for x in visits]
-
-    output = 'Last 10 visits:\n{}'.format('\n'.join(results))
-
-    return output, 200, {'Content-Type': 'text/plain; charset=utf-8'}
-# [END example]
-
+@app.route('/list')
+def list():
+    users = models.User.query.all()
+    return render_template('list.html', users=users)
 
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
-
-if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    return 'An internal error occurred.', 500
