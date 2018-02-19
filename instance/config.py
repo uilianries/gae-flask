b7c98b0214eff819599027530f340c8105f16ae1
Uilian Ries <uilianries@gmail.com>
2018-02-18 21:20:14 -0300

    Add config file
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ instance/config.py
@@ -0,0 +1,31 @@
+##########################################################
+#
+# This is a sample flask.cfg for developing the Flask Recipe App.
+#
+##########################################################
+import os
+
+
+# grab the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
+
+# Update later by using a random number generator and moving
+# the actual key outside of the source code under version control
+SECRET_KEY = 'secret_key'
+WTF_CSRF_ENABLED = True
+DEBUG = True
+
+# SQLAlchemy
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# Bcrypt algorithm hashing rounds
+BCRYPT_LOG_ROUNDS = 15
+
+# Uploads
+UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/app/static/img/'
+UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
+
+UPLOADED_IMAGES_DEST = TOP_LEVEL_DIR + '/app/static/img/'
+UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
